d30a4538c31a89f1cee5d7c428de1ccc6ad944b4
AndySouth <southandy@gmail.com>
2023-07-18 18:46:46 +0100

    use_import_from("dplyr","filter") use_import_from("dplyr","left_join") use_import_from("dplyr","select") use_import_from("dplyr","collect")
    use_import_from("stringr","str_detect")
    use_import_from("stringr","str_to_lower")

---
+++ R/caboodler-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr collect
+#' @importFrom dplyr filter
+#' @importFrom dplyr left_join
+#' @importFrom dplyr select
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_to_lower
+## usethis namespace: end
+NULL
