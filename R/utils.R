57d4ca4c8cc0710ba775c802aa56cdadb54c30b6
AndySouth <southandy@gmail.com>
2023-07-18 18:23:02 +0100

    add dynamic_by()

---
+++ R/utils.R
@@ -0,0 +1,14 @@
+#' Produces a list from string variables and/or strings suitable for use in join expressions
+#' BEWARE function also specified in omop_es
+#' @param lhs left hand side of join expression
+#' @param rhs right hand side of join expression
+#' @examples
+#' df1 <- data.frame(concept_id=999L,concept_name="good drug")
+#' df2 <- data.frame(drug_concept_id=999L,patient_id=666L)
+#' name2 <- drug_concept_id
+#' df3 <- df1 |> left_join(df2,by=dynamic_by("concept_id",name2))
+dynamic_by <- function(lhs,rhs) {
+  res <- c()
+  res[lhs] <- rhs
+  res
+}
